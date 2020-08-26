b29623fd5d3af4a32a4b21d7597377e9c4f5fa38
jundic <jundi_chen@sfu.ca>
2020-08-26 14:02:42 -0700

    Updates

---
+++ precache-manifest.ea40bab3867b019d2852ebbd99a5302b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f95211498a0d71dc948b7047c3178a8",
+    "url": "/firstReactApp/index.html"
+  },
+  {
+    "revision": "7225f28344579f2499b7",
+    "url": "/firstReactApp/static/js/2.2c86764e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/firstReactApp/static/js/2.2c86764e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0581b5453832103032a9",
+    "url": "/firstReactApp/static/js/main.510680d6.chunk.js"
+  },
+  {
+    "revision": "0cda1a4ed522f077d153",
+    "url": "/firstReactApp/static/js/runtime-main.42560f34.js"
+  }
+]);
