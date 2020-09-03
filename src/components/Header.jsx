be09e7ddba814ba27d7904fbb813486767f3a979
jundic <jundi_chen@sfu.ca>
2020-09-03 16:52:09 -0700

    simple fixing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className= "header">
-    <h1>Hi! I am <span className="nameColor">Eddie Chen</span>. This is My <span className="spanJavascriptColor">Javascript</span> , <span className="spanNodejsColor">Node.js</span> and <span className="spanReactColor">React</span> Coding Challenge Page</h1>
-  <h2 >I plan to make 1 simple website a day to horn my <span className="spanReactColor">React</span> and <span className="spanNodejsColor">Node.js</span> skills, starting from <span className= "nameColor">Aug 26, 2020</span>, game on!</h2>
+    <h1>Hi! I am <span className="nameColor">Eddie Chen</span>. This is My <span className="spanJavascriptColor">Javascript</span> , <span className="spanNodejsColor">Node.js</span> and <span className="spanReactColor">React</span> Project Page</h1>
     </div>
   );
 
